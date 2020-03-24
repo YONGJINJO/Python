c63145ff72e5143896780425bec95ad726de82cf
YONGJINJO <49581644+YONGJINJO@users.noreply.github.com>
2020-03-24 19:35:19 +0900

    review about hash table

--- Data_Structure/Hash_Table/review.py
+++ Data_Structure/Hash_Table/review.py
@@ -1,28 +1,30 @@
 class Node:
-    """해쉬테이블을 위한 노드 클래스"""
+    """노드 클래스"""
     def __init__(self, key, value):
         self.key = key
         self.value = value
-        self.next = None
         self.prev = None
+        self.next = None
 
 class LinkedList:
-    """링크드 리스트 클래스"""
+    """더블리 링크드리스트 클래스"""
     def __init__(self):
         self.head = None
         self.tail = None
 
     def find_node_with_key(self, key):
-        """key를 이용하여 원하는 node를 찾는 함수, node가 존재하지 않으면 None 리턴"""
+        """key를 이용해 원하는 노드를 찾는 클래스"""
         iterator = self.head
-
         while iterator is not None:
             if iterator.key == key:
+
                 return iterator
             iterator = iterator.next
+
         return None
 
     def append(self, key, value):
+        """링크드 리스트 맨 뒤에 새로운 노드를 추가하는 클래스"""
         new_node = Node(key, value)
         if self.head is None:
             self.head = new_node
@@ -32,29 +34,95 @@ class LinkedList:
             new_node.prev = self.tail
             self.tail = new_node
 
-    def delete(self, node_to_delete):
-        """지정된 노드를 삭제하는 메소드, 삭제한 노드의 value를 리턴"""
-        if node_to_delete == self.head and node_to_delete == self.tail: #지우려는 노드가 마지막으로 남은 노드일 경우
+    def delete(self, node_to_delelte):
+        """원하는 노드를 삭제하는 클래스"""
+        data = node_to_delelte.value
+        if node_to_delelte is self.head and node_to_delelte is self.tail:
             self.head = None
             self.tail = None
-        elif node_to_delete == self.head: # 지우려는 노드가 head 노드일 경우
-            self.head.next = self.head
+        elif node_to_delelte is self.head:
+            self.head = self.head.next
             self.head.prev = None
-        elif node_to_delete == self.tail: # 지우려는 노드가 tail 노드일 경우
-            self.tail.prev = self.tail
+        elif node_to_delelte is self.tail:
+            self.tail = self.tail.prev
             self.tail.next = None
-        else: # 지우려는 노드가 사이에 존재하는 임의의 노드일 경우
-            node_to_delete.prev.next = node_to_delete.next
-            node_to_delete.next.prev = node_to_delete.prev
+        else:
+            node_to_delelte.prev.next = node_to_delelte.next
+            node_to_delelte.next.prev = node_to_delelte.prev
 
-        return node_to_delete.value
+        return data
 
     def __str__(self):
-        """링크드 리스트를 문자열로 표현해서 리턴"""
-
         res_str = ""
         iterator = self.head
+
         while iterator is not None:
-            res_str += "{} : {}".format(iterator.key, iterator.value)
+            res_str += "{} : {}\n".format(iterator.key, iterator.value)
             iterator = iterator.next
+
         return res_str
+
+class HashTable:
+    def __init__(self, capacity):
+        self._capacity = capacity
+        self._table = [LinkedList() for _ in range(self._capacity)]
+
+    def _hash_function(self, key):
+        return hash(key) % self._capacity
+
+    def _get_linked_list_for_key(self, key):
+        hashed_key = self._hash_function(key)
+        return self._table[hashed_key]
+
+    def _look_up_node(self, key):
+        linked_list = self._get_linked_list_for_key(key)
+        return linked_list.find_node_with_key(key)
+
+
+    def look_up_value(self, key):
+        return self._look_up_node(key).value
+
+
+    def insert(self, key, value):
+
+        temp = self._look_up_node(key)
+
+        if temp is not None:
+            temp.value = value
+        else:
+            self._get_linked_list_for_key(key).append(key, value)
+
+    def delete_by_key(self, key):
+        linked_list = self._get_linked_list_for_key(key)
+        temp = self._look_up_node(key)
+        if temp is not None:
+            linked_list.delete(temp)
+
+    def __str__(self):
+        res_str = ""
+
+        for linked_list in self._table:
+            res_str += str(linked_list)
+
+        return res_str[:-1]
+
+
+test_scores = HashTable(50) # 시험 점수를 담을 해시 테이블 인스턴스 생성
+
+# 여러 학생들 이름과 시험 점수 삽입
+test_scores.insert("현승", 85)
+test_scores.insert("영훈", 90)
+test_scores.insert("동욱", 87)
+test_scores.insert("지웅", 99)
+test_scores.insert("신의", 88)
+test_scores.insert("규식", 97)
+test_scores.insert("태호", 90)
+
+# 학생들 시험 점수 삭제
+test_scores.delete_by_key("태호")
+test_scores.delete_by_key("지웅")
+test_scores.delete_by_key("신의")
+test_scores.delete_by_key("현승")
+test_scores.delete_by_key("규식")
+
+print(test_scores)
